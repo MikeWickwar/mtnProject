af8ad33436a33e94d9196ae763c2a30b6c0e74d7
Mike Wickwar <mike.wickwar@gmail.com>
2015-12-14 22:50:28 -0700

    menu working 2nd page linked, second page css started

--- js/dom.js
+++ js/dom.js
@@ -1,24 +1,37 @@
 //tests working then a call
 console.log('working js');
-//call for brek
+//call for brekenridge from weather api
 var getter = $.ajax({
   url: "http://www.myweather2.com/developer/weather.ashx?uac=E8tRYOl8xe&uref=65ae20fe-d8fd-4fb7-b9cc-8262c85bf223&output=json",
   method: "GET",
   dataType: "json"
 });
+/// api key for google AIzaSyBl0CTALhcPRBiyO4IAYGaqdoXq5UjYrK4
+var dGetter = $.ajax({
+  url: "https://maps.googleapis.com/maps/api/distancematrix/json?origins=Denver&destinations=Brekenridge&key=AIzaSyBl0CTALhcPRBiyO4IAYGaqdoXq5UjYrK4",
+  method: "GET",
+  dataType: "json"
+});
+
 
 getter.done(function(response){
   console.log(response);
 });
-
-
+dGetter.done(function(response){
+  console.log(response);
+});
 // real dom interaction starts here
-var motdBtn = document.getElementById('motdBtn');
-  $('#motdBtn').one('click',function(){
+  $('.info').css('display','none')
+  $('#motdBtn').click(function(){
     console.log('click');
-  $('#mtnB').append("<div class='box info'><h3>This</h3></div>");
-  $('#mtnB').append("<div class='box info'><h3>is</h3></div>");
-  $('#mtnB').append("<div class='box info'><h3>where</h3></div>");
-  $('#mtnB').append("<div class='box info'><h3>To</h3></div>");
-  $('#mtnB').append("<div class='box info'><h3>display info</h3></div>");
+  $('.togglers1').toggle(800,"swing");
+  $('.togglers2').toggle(1000,"swing");
+  $('.togglers3').toggle(1200,"swing");
+  $('.togglers4').toggle(1400,"swing");
+  $('.togglers5').toggle(1600,"swing");
+
+})
+$('#motdBtn').on('click',function(){
+  $('.info').css('display','block');
+
 })
